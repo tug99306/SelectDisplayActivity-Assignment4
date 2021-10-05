466e6590db858db32e6a77ea40711e1deb406645
Saleem Hussein <tug99306@temple.edu>
2021-10-04 23:32:10 -0400

    Implement DisplayActivity.kt and update MainActivity.kt to send info back to DisplayActivity.kt when an image is clicked

--- app/src/main/java/edu/temple/selectactivity/DisplayActivity.kt
+++ app/src/main/java/edu/temple/selectactivity/DisplayActivity.kt
@@ -1,11 +1,71 @@
 package edu.temple.selectactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+        val titleTextView = findViewById<TextView>(R.id.textViewText)
+        val imgView = findViewById<ImageView>(R.id.imageViewDisplay)
+
+        val imgDesc = intent.getStringExtra("DESCRIPTION")
+        val imgId = intent.getIntExtra("IMAGE_ID",0)
+
+
+
+        // Create an intent to pass information to calling activity
+        val resultIntent = Intent().putExtra("resultValue", "Second activity closed with Back button")
+        // Set code and data for returned result
+        setResult(RESULT_OK, resultIntent)
+
+        titleTextView.text = imgDesc
+        imgView.setImageResource(imgId)
+
+        findViewById<Button>(R.id.buttonClose).setOnClickListener {
+
+            // Replace Extra information in intent
+            resultIntent.putExtra("resultValue", "Second activity closed with finish()")
+            // Set code and data for returned result
+            setResult(RESULT_OK, resultIntent)
+            // Close activity
+            finish()
+        }
+
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Second activity state", "onStart() fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Second activity state", "onResume() fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Second activity state", "onPause() fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Second activity state", "onStop() fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Second activity state", "onDestroy() fired")
+    }
+
+
 }
--- app/src/main/java/edu/temple/selectactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/selectactivity/MainActivity.kt
@@ -1,9 +1,10 @@
 package edu.temple.selectactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.GridLayoutManager
 import edu.temple.selectactivity.ImageAdapter
@@ -11,41 +12,86 @@ import edu.temple.selectactivity.ImageObject
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val imageAdapter: ImageAdapter
-        val recycler = findViewById<RecyclerView>(R.id.recycler)
-        //val nameTextView = findViewById<TextView>(R.id.nameText)
-        //val displayImageView = findViewById<ImageView>(R.id.displayImageView)
+    val secondActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        Log.d("Returned data", it.data?.getStringExtra("resultValue").toString())
+    }
 
-        val imageArray = arrayOf(
-            ImageObject("Eagle", R.drawable.eagle)
-            , ImageObject("Hawk", R.drawable.hawk)
-            , ImageObject("Lovebird", R.drawable.lovebird)
-            , ImageObject("Stella the Owl", R.drawable.owlstellatu)
-            , ImageObject("Parakeet", R.drawable.parakeet)
-            , ImageObject("Parrot", R.drawable.parrot)
-            , ImageObject("Penguin", R.drawable.penguin)
-            , ImageObject("Puffin", R.drawable.puffin)
-            , ImageObject("Strawberry Finch", R.drawable.strawberryfinch)
-            , ImageObject("Turtle Dove", R.drawable.turtledove)
-        )
-        fun imageItemClicked(images : ImageObject) {
-            //nameTextView.text = images.description
-            //displayImageView.setImageResource(images.imageId)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val secondActivityIntent = Intent(this, DisplayActivity::class.java)
+
+            val imageAdapter: ImageAdapter
+            val recycler = findViewById<RecyclerView>(R.id.recycler)
+
+            val imageArray = arrayOf(
+                ImageObject("Eagle", R.drawable.eagle)
+                , ImageObject("Hawk", R.drawable.hawk)
+                , ImageObject("Lovebird", R.drawable.lovebird)
+                , ImageObject("Stella the Owl", R.drawable.owlstellatu)
+                , ImageObject("Parakeet", R.drawable.parakeet)
+                , ImageObject("Parrot", R.drawable.parrot)
+                , ImageObject("Penguin", R.drawable.penguin)
+                , ImageObject("Puffin", R.drawable.puffin)
+                , ImageObject("Strawberry Finch", R.drawable.strawberryfinch)
+                , ImageObject("Turtle Dove", R.drawable.turtledove)
+            )
+            fun imageItemClicked(images : ImageObject) {
+                val name = images.description
+                val img = (images.imageId)
+                secondActivityIntent.putExtra("DESCRIPTION", name)
+                secondActivityIntent.putExtra("IMAGE_ID", img)
+                startActivity(secondActivityIntent)
+                //secondActivityLauncher.launch(secondActivityIntent)
+
+            }
+
+            recycler.layoutManager = GridLayoutManager(this,2)
+            imageAdapter = ImageAdapter(this, imageArray){ imagesItem : ImageObject ->
+                imageItemClicked(imagesItem)}
+            recycler.adapter = imageAdapter
         }
 
-        recycler.layoutManager = GridLayoutManager(this,2)
-        imageAdapter = ImageAdapter(this, imageArray){ imagesItem : ImageObject ->
-            imageItemClicked(imagesItem)}
-        recycler.adapter = imageAdapter
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Overriding onRestoreInstanceState() allows you to separate your "initialization" code
+        // from your "restoration" code.
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // This method is used to persist state data before the activity is destroyed by Android,
+        // allowing it to be retrieved when the activity is recreated.
+        // Please items in the Bundle argument and they will be returned when onCreate() and
+        // onRestoreInstanceState() are called.
+        //
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("Main activity state", "onStart() fired")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d("Main activity state", "onResume() fired")
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d("Main activity state", "onPause() fired")
+    }
 
-}
+    override fun onStop() {
+        super.onStop()
+        Log.d("Main activity state", "onStop() fired")
+    }
 
-}
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Main activity state", "onDestroy() fired")
+    }
+
+    }
+
